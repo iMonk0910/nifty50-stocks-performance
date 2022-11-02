3d0d5cea6ce2c516f96a1a6e4204f556da799849
PR <79361243+iMonk0910@users.noreply.github.com>
2022-11-03 05:16:09 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -264,7 +264,6 @@ def nifty50def(startDate, endDate, total_investment, Investment_time_in_years):
 
 Nifty50 = nifty50def(startDate,endDate, total_investment,Investment_time_in_years)
 
-
 # Group data together
 import altair as alt
 plot_data = [Equal_Alloc_Buy_Hold[3], Nifty50[3], performance_Strat[3]]
@@ -282,18 +281,20 @@ df_plot.set_index = DateIndex.values
 fig, ax = plt.subplots(figsize=(10, 8))
 ax.plot(df_plot["Equal_Alloc_Buy_Hold"], label='Equal_Alloc_Buy_Hold')  # Plot some data on the axes.
 ax.plot(df_plot["Nifty50"], label='Nifty50')  # Plot more data on the axes...
-ax.plot(df_plot["performance_Strat"], label='performance_Strat')  # ... and some more.
+ax.plot(df_plot["performance_Strat"], label='Performance_Strat')  # ... and some more.
 ax.set_xlabel('Date')  # Add an x-label to the axes.
 ax.set_ylabel('')  # Add a y-label to the axes.
-ax.set_title("Simple Plot")  # Add a title to the axes.
+ax.set_title("Sample Plot")  # Add a title to the axes.
+ax.legend() # Add a legend.
+ax.set_xticklabels(["2020-10", "2021-01", "2021-04", "2021-07", "2021-10", "2022-01", "2022-04", "2022-07","2022-10"],rotation = 45)
 
-ax.set_xticks(["2020-10", "2021-01", "2021-04", "2021-10", "2022-07","2022-12"])
+st.plot(fig)
 st.pyplot(fig)
 
 df_performance = pd.DataFrame()
 df_performance["Index"] = ["Equal_Alloc_Buy_Hold", "Nifty50", "Performance_Strat"]
 df_performance["CAPR%"] = [Equal_Alloc_Buy_Hold[0], Nifty50[0], performance_Strat[0]]
-df_performance["Volatility"] = [Equal_Alloc_Buy_Hold[1], Nifty50[1], performance_Strat[1]]
+df_performance["Volatility%"] = [Equal_Alloc_Buy_Hold[1], Nifty50[1], performance_Strat[1]]
 df_performance["Sharp%"] = [Equal_Alloc_Buy_Hold[2], Nifty50[2], performance_Strat[2]]
 df_performance.set_index("Index")
 
