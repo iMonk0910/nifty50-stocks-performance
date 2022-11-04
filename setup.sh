1115351cde921a4fb1a4828a79c42beb6a1fa98a
PR <79361243+iMonk0910@users.noreply.github.com>
2022-11-04 22:44:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"prdixit2503@gmail.com\"\n\
+echo "\\
+[general]\\n\\
+email = \\"prdixit2503@gmail.com\\"\\n\\
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "\\
+[server]\\n\\
+headless = true\\n\\
+enableCORS=false\\n\\
+port = $PORT\\n\\
